62a0868a323b805520c0c852af877ca826f2ac65
Salman Akhlaqi <salman.akhlaqi@ontario.ca>
2022-01-25 11:00:40 -0500

    update files

--- assignment/pages/api/funds.ts
+++ assignment/pages/api/funds.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
--- assignment/pages/index.tsx
+++ assignment/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import useSWR from 'swr'
-import { PrismaClient, Funds, Prisma } from '@prisma/client';
+import { PrismaClient, Funds } from '@prisma/client';
 import _ from 'lodash';
 
 let formData = {}
